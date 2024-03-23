af46677218c1f141db3dfbd5c88881c58c9a4c89
corvus5e <corvus5e@gmail.com>
2024-03-23 15:38:15 +0200

    Exercise 4-2. But using pointers before they presented in the book to reuse function

--- ex_4_2.c
+++ ex_4_2.c
@@ -1,21 +1,53 @@
+/*
+ * Exercise 4-2. Extend atof to handle scientific notation of the form 123.45e-6
+ * where a floating-point number may be followed by e or E and an optionally signed exponent.
+ */
 
 #include <stdio.h>
 #include <ctype.h>
+#include <math.h>
 
 /* atof: convert string s to double */
 double atof(char s[]);
 
+/*atof_ex: converts string to a double and
+ * puts into `non_digit_pos` the index of the firts non digit char
+ * after the float number
+ * */
+double atof_ex(char s[], int* non_digit_pos);
+
 int main()
 {
     char* str = "-123.456";
-    printf("The converted value is %f", atof(str)); 
+    printf("The converted float value is %f\n", atof(str));
+
+    char* str2 = "-123.45e+1.5";
+    printf("The converted value scientific float value is %f\n", atof(str2));
+
 }
 
 double atof(char s[])
 {
-    double val, power;
-    int i, sign;
+    double num;
+    double exp; // according to the task the exponent can be float
+    int i = 0;
 
+    num = atof_ex(s, &i);
+
+    if(s[i] == 'e' || s[i] == 'E')
+    {
+        exp = atof_ex(s + i+1, &i);
+        num *= pow(10, exp);
+    }
+
+    return num;
+}
+
+double atof_ex(char s[], int* non_digit_pos)
+{
+    double val;
+    double power = 1.0;
+    int i, sign;
     for (i = 0; isspace(s[i]); i++) /* skip white space */
         ;
 
@@ -34,6 +66,8 @@ double atof(char s[])
         val = 10.0 * val + (s[i] - '0');
         power *= 10;
     }
+
+    *non_digit_pos = i;
 
     return sign * val / power;
 }
