af7c6e5cd87303deabc016bfe6d555cb9cadbfa2
corvus5e <corvus5e@gmail.com>
2024-07-10 23:34:01 +0300

    Exercise 5-9. No indexing at all. Looks ugly)

---
+++ ex_5_9.c
@@ -0,0 +1,63 @@
+/*
+ * Exercise 5-9. Rewrite the routines day_of_year and month_day with pointers instead of
+ * indexing.
+ * */
+
+#include <stdio.h>
+
+int day_of_year(int year, int month, int day);
+void month_day(int year, int year_day, int *month, int *day);
+
+int main(){
+    int input_year = 2024;
+    int input_month = 7;
+    int input_day   = 8;
+
+    int year_day = day_of_year(input_year, input_month, input_day);
+    printf("%d\n", year_day);
+
+    int month, day;
+    month_day(input_year, year_day, &month, &day);
+    printf("%d, %d\n", month, day);
+}
+
+static char daytab[2][13] = {
+    {0, 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31},
+    {0, 31, 29, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31}
+};
+
+int day_of_year(int year, int month, int day){
+    int leap = (year%4 == 0 && year%100 != 0) || year%400 == 0;  
+
+    if(month < 0 || month > 13 || day < 1 || *(*daytab + sizeof(*daytab)*leap + month) < day)
+        return -1;
+
+    for(;--month > 0; day += *(*daytab + sizeof(*daytab)*leap + month))
+        ;
+
+    return day;
+}
+
+void month_day(int year, int year_day, int *month, int *day){
+    if(year < 0){
+        *month = -1;
+        *day = -1;
+        return;
+    }
+
+    int leap = (year%4 == 0 && year%100 != 0) || year%400 == 0;   
+
+    if(year_day < 1 || year_day > 365 + leap){
+        *month = -1;
+        *day = -1;
+        return;
+    }
+
+    *month = 0;
+    *day = year_day;
+    while(*day > *(*daytab + sizeof(*daytab)*leap + (++(*month)))){
+        *day -= *(*daytab + sizeof(*daytab)*leap + *month);
+    }
+}
+
+
