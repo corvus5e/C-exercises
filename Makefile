50ac337690985b6fc25193a1d89af6a77e2cd52d
corvus5e <corvus5e@gmail.com>
2024-09-27 00:02:24 +0300

    Returning back to C, starting 5-13. It ain't much, but it's honest work

--- Makefile
+++ Makefile
@@ -1,4 +1,4 @@
-files = ex_5_10.c #$(wildcard *.cpp)
+files = ex_5_13.c #$(wildcard *.cpp)
 target := a.out
 
 run: main
@@ -6,7 +6,7 @@ run: main
 
 main: $(files)
 	#clear
-	gcc --std=c17 -O0 -Wall -pg $(files) -lm -o $(target)
+	gcc --std=c17 -O0 -Wall -ggdb $(files) -lm -o $(target)
 clean:
 	echo "Cleaning"
 	rm -f *.out
--- ex_1_20.c
+++ ex_1_20.c
@@ -1,3 +1,16 @@
+/*
+ * Exercise 1-20. Write a program detab that replaces tabs in the input with the proper number
+ * of blanks to space to the next tab stop. Assume a fixed set of tab stops, say every n columns.
+ * Should n be a variable or a symbolic parameter?
+*/
+
+/*
+ * Make ex_1_20.c and then compare:
+ * cat data/ex_1_20_input.txt
+ * with
+ * ex_1_20.out < data/ex_1_20_input.txt
+*/
+
 #include <stdio.h>
 
 #define TAB_LEN 15
