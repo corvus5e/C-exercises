9af43b62ba6a1086b493601829cd39e15efedd8b
corvus5e <corvus5e@gmail.com>
2024-09-27 23:21:14 +0300

    Exercise 5-13

--- ex_5_13.c
+++ ex_5_13.c
@@ -8,14 +8,50 @@
 * fixed size.
 */
 
+#define _GNU_SOURCE
 #include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 
-#define MAX_LINES_NUM 10
+#define DEFAULT_LINES_NUM 10
 
-int main(void){
-	char* lines[MAX_LINES_NUM] = {0};
-	for (int i = 0; i < MAX_LINES_NUM ; ++i)
+int main(int argc, char* argv[]){
+	int n = DEFAULT_LINES_NUM;
+	if(argc > 1){
+		int tmp_n = atoi(argv[1]);
+		if(tmp_n > 0)
+			n = tmp_n;
+	}
+
+	char** lines = malloc(n * sizeof(char*));
+	size_t* lengths = malloc(n * sizeof(size_t));
+
+	for(int i = 0; i < n; ++i){
+		lines[i] = NULL;
+		lengths[i] = 0;
+	}
+
+	ssize_t nread = 0;
+	int curr_line = 0;
+
+	while((nread = getline(lines + curr_line, lengths + curr_line, stdin)) != -1){
+		++curr_line;
+		curr_line %= n;
+	}
+
+	for(int i = curr_line; i < n; ++i)
 		if(lines[i] != NULL)
-			printf("%s\n", lines[i]);
+			printf("%s", lines[i]);
+
+	for(int i = 0; i < curr_line; ++i)
+		printf("%s", lines[i]);
+
+	for(int i = 0; i < n; ++i)
+		if(lines[i] != NULL)
+			free(lines[i]);
+
+	free(lines);
+	free(lengths);
+	
 	return 0;
 }
