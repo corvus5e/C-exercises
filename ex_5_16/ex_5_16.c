f42255b508fd1f4b0951c9560189098a04cb148a
corvus5e <corvus5e@gmail.com>
2025-01-14 00:40:29 +0100

    Exercise 5-16. Adding task description

--- ex_5_16/ex_5_16.c
+++ ex_5_16/ex_5_16.c
@@ -1,6 +1,6 @@
 /*
- * Exercise 5-15. Add the option -f to fold upper and lower case together, so that case
- * distinctions are not made during sorting; for example, a and A compare equal.
+ * Exercise 5-16. Add the -d (``directory order'') option, which makes comparisons only on
+ * letters, numbers and blanks. Make sure it works in conjunction with -f.
 */
 
 #define _GNU_SOURCE
