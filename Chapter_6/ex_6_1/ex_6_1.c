830958aa9d0781ea2ceda12f5f7fc70406ad7509
corvus5e <corvus5e@gmail.com>
2025-01-27 01:02:20 +0100

    Exercise 6-1. Comments and underscore handled

--- Chapter_6/ex_6_1/ex_6_1.c
+++ Chapter_6/ex_6_1/ex_6_1.c
@@ -3,6 +3,11 @@
  * comments, or preprocessor control lines. Write a better version.
 */
 
+/* TODO:
+   - string constants
+   - preprocessor control lines
+*/
+
 #include <stdio.h>
 #include <ctype.h>
 #include <string.h>
@@ -10,43 +15,25 @@
 struct key {
 	char *word;
 	int count;
-} keytab[] = {
-	{"auto", 0},
-	{"break", 0},
-	{"case", 0},
-	{"char", 0},
-	{"const", 0},
-	{"continue", 0},
-	{"default", 0},
-	{"do", 0},
-	{"double", 0},
-	{"else", 0},
-	{"enum", 0},
-	{"extern", 0},
-	{"float", 0},
-	{"for", 0},
-	{"goto", 0},
-	{"if", 0},
-	{"int", 0},
-	{"struct", 0},
-	{"long", 0},
-	{"switch", 0},
-	{"register", 0},
-	{"typedef", 0},
-	{"return", 0},
-	{"union", 0},
-	{"short", 0},
-	{"unsigned", 0},
-	{"signed", 0},
-	{"void", 0},
-	{"sizeof", 0},
-	{"volatile", 0},
-	{"static", 0},
-	{"while", 0},
-};
+} keytab[] = { {"auto", 0},     {"break", 0},   {"case", 0},   {"char", 0},     {"const", 0},
+			   {"continue", 0}, {"default", 0}, {"do", 0},     {"double", 0},   {"else", 0},
+			   {"enum", 0},     {"extern", 0},  {"float", 0},  {"for", 0},      {"goto", 0},
+			   {"if", 0},       {"int", 0},     {"long", 0},   {"register", 0}, {"return", 0},
+			   {"short", 0},    {"signed", 0},  {"sizeof", 0}, {"static", 0},   {"struct", 0},
+			   {"switch", 0},   {"typedef", 0}, {"union", 0},  {"unsigned", 0}, {"void", 0},
+			   {"volatile", 0}, {"while", 0}};
 
 #define MAXWORD 100
 #define NKEYS (sizeof keytab / sizeof(keytab[0]))
+
+#define EMPTY_FLAGS   0
+#define BLOCK_COMMENT 1
+#define LINE_COMMENT  2
+#define STRING_CONST  4
+#define PREPROCESSOR  8
+
+char flags_s = EMPTY_FLAGS;
+
 int getword(char *, int);
 int binsearch(char *, struct key *, int);
 
@@ -94,26 +81,53 @@ int binsearch(char *word, struct key tab[], int n)
 /* getword: get next word or character from input */
 int getword(char *word, int lim)
 {
-	int c, getch(void);
+	int getch(void);
 	void ungetch(int);
 
 	char *w = word;
+	*w = '\0'; // consider the buffer as empty at start
+	int c = 0;
 
-	while (isspace(c = getch()))
-		;
-	if (c != EOF)
-		*w++ = c;
+	while(c != EOF && !word[0]){
 
-	if (!isalpha(c)) {
-		*w = '\0';
-		return c;
-	}
+		while (isspace(c = getch()))
+			;
 
-	for ( ; --lim > 0; w++)
-		if (!isalnum(*w = getch())) {
-			ungetch(*w);
-			break;
+		if(c == EOF)
+			return EOF;
+
+		if(c == '/'){ // possible comment section
+			// TODO: make separate function, for example skip_till(char* end_pattern);
+			if((c = getch()) == '/'){ // this is line comment
+				while((c = getch()) != EOF && c != '\n')
+					;
+				continue; // comment line is finished, no worlds, start from the beginnig
+			}
+			else if(c == '*'){ // this is a block comment
+				char b = 0;
+				c = getch();
+				while(b != EOF && c != EOF && !(b == '*' && c == '/')){
+					b = c;
+					c = getch();
+				}
+				continue; // comment block is finished, no worlds, start from the beginnig
+			}
 		}
+
+		if (c != EOF)
+			*w++ = c;
+
+		if (!isalpha(c)) {
+			*w = '\0';
+			return c;
+		}
+
+		for ( ; --lim > 0; w++)
+			if (!isalnum(*w = getch()) && *w != '_') {
+				ungetch(*w);
+				break;
+			}
+	}
 
 	*w = '\0';
 
--- Chapter_6/ex_6_1/sample_program.c
+++ Chapter_6/ex_6_1/sample_program.c
@@ -1,4 +1,24 @@
+#if FOO
+#endif
+
 int main()
 {
+	// /* struct S {};*/
+	/*return*/
+	/*/  *
+#if FOO2
+#endif
+	   struct S {};
+	   // return -1;
+
+		if(1){
+			double a;
+		}
+	*/
+	// if(0) int x = 0;
+
+	char* char_ = "double hello!";
+	char char_var;
+	long/*long*/d;//
 	return 0;
 }
