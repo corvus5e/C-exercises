d47680d4d511c5ced3429e227791acfa259453bc
corvuse <corvus5e@gmail.com>
2025-03-01 00:04:38 +0100

    Exercise 6-6. Def values handle one line C code

--- Chapter_6/ex_6_6/ex_6_6.c
+++ Chapter_6/ex_6_6/ex_6_6.c
@@ -12,6 +12,14 @@
 #define MAXWORD 100
 #define HASHSIZE 101
 
+enum word_kind {
+	SPACE,
+	WORD,
+	LITERAL,
+	COMMENT,
+	NEW_LINE = '\n',
+};
+
 struct nlist {
 	struct nlist *next;
 	char *name;
@@ -28,6 +36,7 @@ void print_hashtab();
 unsigned int hash(char*);
 
 int getword(char *, int);
+int get_next_word(char*, int); // skips spaces and comments
 int read_till_char(char*, char stop_char, int lim);
 int read_defn(char *, int);
 int skip_stdin_till(const char* pattern);
@@ -43,21 +52,22 @@ int main()
 
 	while((c = getword(word, MAXWORD)) != EOF){
 		if(!strcmp(word, "#define")){
-			if(getword(word, MAXWORD) != EOF && read_defn(value, MAXWORD) != EOF){
-			//	printf("%s", word);
+			if(get_next_word(word, MAXWORD) != EOF &&
+				read_defn(value, MAXWORD) != EOF){
+				//printf("[%s=%s]", word, value);
 				install(word, value);
 			}
 		}
 		else if(isalpha(word[0]) && (np = lookup(word))){
 			printf("%s", np->defn);
 		}
-		//else if(isspace(c))
-		//	printf("%c", c);
 		else
 			printf("%s", word);
 	}
 
-	//print_hashtab();
+	printf("/* Defines used:\n");
+	print_hashtab();
+	printf("*/\n");
 }
 
 struct nlist *lookup(char *s)
@@ -154,51 +164,58 @@ int getword(char *word, int lim)
 {
 	char *w = word;
 	*w = '\0'; // consider the buffer as empty at start
-	int c = 0;
 
-	while(c != EOF && !word[0]){
-		while (isspace(c = getch())){
-			printf("%c", c);
-			if(c == '\n')
-				return c;
+	int c = getch();
+	
+	if(c == EOF)
+		return EOF;
+
+	if(isspace(c)){
+		for(*w++ = c; (*w = getch()) == c; ++w)
+			;
+		ungetch(*w);
+		*w = '\0';
+		return c == '\n' ? NEW_LINE : SPACE;
+	}
+
+	if(c == '/'){ // possible comment section
+		if((c = getch()) == '/'){ // this is line comment
+			skip_stdin_till("\n");
+			ungetch('\n');
+			return COMMENT;
 		}
-
-		if(c == EOF)
-			return EOF;
-
-		if(c == '/'){ // possible comment section
-			if((c = getch()) == '/'){ // this is line comment
-				skip_stdin_till("\n");
-				ungetch('\n');
-				continue;
-			}
-			else if(c == '*'){ // this is a block comment
-				skip_stdin_till("*/");
-				continue;
-			}
-			ungetch(c);
-			c = '/';
+		else if(c == '*'){ // this is a block comment
+			skip_stdin_till("*/");
+			return COMMENT;
 		}
-		else if(c == '"' || c == '\''){
-			*w++ = c;
-			return read_till_char(w, c, MAXWORD - 1);
-		}
-
+		ungetch(c);
+		c = '/';
+	}
+	else if(c == '"' || c == '\''){
 		*w++ = c;
+		read_till_char(w, c, MAXWORD - 1);
+		return LITERAL;
+	}
 
-		if(!isalnum(c) && c != '#') //write # and directive in one word
-			break;
+	*w++ = c;
 
-		for ( ; --lim > 0; w++)
-			if (!isalnum(*w = getch()) && *w != '_') {
-				ungetch(*w);
-				break;
-			}
+	if(isalnum(c) || c == '#'){ //write # and directive in one word
+		for(;--lim > 0 && (isalnum((*w = getch())) || *w == '_'); ++w)
+			;
+		ungetch(*w);
 	}
 
 	*w = '\0';
 
-	return word[0];
+	return WORD;
+}
+
+int get_next_word(char *word, int lim)
+{
+	int c;
+	while((c = getword(word, lim)) != EOF && (c == SPACE || c == COMMENT))
+		;
+	return c;
 }
 
 int read_till_char(char *buf, char stop_char, int lim)
@@ -225,13 +242,21 @@ int read_defn(char *defn, int lim)
 	int c;
 	char word[MAXWORD];
 
-	while(lim > 0 && (c = getword(word, lim)) != EOF && c != '\n'){
+	if((c = get_next_word(defn, lim)) == EOF)
+		return c;
+
+	for(;*defn;++defn) // skip the first written word
+		;
+	
+	while(lim > 0 && (c = getword(word, lim)) != EOF && c != NEW_LINE){
 		//printf("===>%s\n", word);
 		char *w = &word[0];
 		for(; --lim && (*defn = *w); ++defn, ++w)
 			;
 		*defn = '\0';
 	}
+	if(c == EOF)
+		ungetch(c);
 
 	return c;
 }
@@ -267,7 +292,6 @@ int skip_stdin_till(const char* pattern)
 			free(buf);
 			return 0;
 		}
-		//printf("buf:%s\n", buf);
 	}
 
 	return 1;
@@ -285,8 +309,10 @@ int getch(void)
 
 void ungetch(int c) /*push character back to input*/
 {
-	if(bufp >= BUFERSIZE)
+	if(bufp >= BUFERSIZE){
 		printf("Error: ungetch - too many charackters\n");
+		exit(1);
+	}
 	else
 		buf[bufp++] = c;
 }
--- Chapter_6/ex_6_6/sample_pogram.c
+++ Chapter_6/ex_6_6/sample_pogram.c
@@ -5,17 +5,17 @@
 #define LOG /*This is comment*/printf("I was here!\n");
 #define  /*Comment*/	   LOG2 	  printf( "And I was here" );     
 #define MSG "Hello from define\n"
-#define LOOP {for(int i = 0; i < 10/*Ten*/; ++i){printf("%d ", i);}}
+#define LOOP /*Lets put code here:*/{for(int i = 0; i < 10/*Ten*/; ++i){printf("%d ", i);}}
 
 static int arr[N];
 
-#define N 777
+#define N 7
 
 int main()
 {
 	char* msg = "Hello, \"world!\"";
 	printf("%d %s\n", COUNT, MSG);
-	int a = 5 / 4*N;	// This is a
+	float a = 5.f / 4*N;	// This is a
 	int b=3/2;
 	int c=3*COUNT/a;
 	/*Comment
