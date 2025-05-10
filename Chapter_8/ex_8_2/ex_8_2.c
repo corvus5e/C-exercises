f2587671acffac7b841777263e4faaaa800c6c74
corvus5e <corvus5e@gmail.com>
2025-05-10 21:57:40 +0200

    Exercise 8-2

--- Chapter_8/ex_8_2/ex_8_2.c
+++ Chapter_8/ex_8_2/ex_8_2.c
@@ -4,17 +4,25 @@
 */
 
 #include <fcntl.h>
+#include <stdlib.h>
 
-#define NULL 	 0
 #define EOF 	 (-1)
-#define BUFSIZE	 1024
+#define BUFSIZE	 10
 #define OPEN_MAX 20 /* max files open at once*/
 
 typedef struct _iobuf {
 	int  ctn;   /* characters left */
 	char *ptr;  /* next character position */
 	char *base; /* location of buffer */
-	int  flag;  /* mode of file access */
+
+	struct {
+		unsigned char read : 1;  /* file open for reading */
+		unsigned char write : 1;  /* file open for writing */
+		unsigned char unbuf : 1;  /* file is unbuffered */
+		unsigned char eof : 1; /* EOF has occured on this file */
+		unsigned char err : 1; /* erro occured on this file */
+	} flags;
+
 	int  fd;    /* file descriptor */
 } FILE;
 
@@ -24,26 +32,14 @@ extern FILE _iob[OPEN_MAX];
 #define stdout (&_iob[1])
 #define stderr (&_iob[2])
 
-enum _flags {
-	_READ  = 01,  /* file open for reading */
-	_WRITE = 02,  /* file open for writing */
-	_UNBUF = 04,  /* file is unbuffered */
-	_EOF   = 010, /* EOF has occured on this file */
-	_ERR   = 020  /* erro occured on this file */
-};
-
 int _fillbuf(FILE*);
-int _flushbuf(FILE*);
-
-#define feof(p) 	(((p)->flag & _EOF) != 0)
-
-#define ferror(p)	(((p)->flag & _ERR) != 0)
+int _flushbuf(int, FILE*);
 
 #define getc(p) 	(--(p)->ctn >= 0 \
 			? (unsigned char) *(p)->ptr++ : _fillbuf(p))
 
 #define putc(x, p)	(--(p)->ctn >= 0 \
-			? (unsigned char) *(p)->ptr++ = (x) : _flushbuf((x), p))
+			? *(p)->ptr++ = (x) : _flushbuf((x), p))
 
 #define getchar()	getc(stdin)
 
@@ -52,6 +48,12 @@ int _flushbuf(FILE*);
 FILE* fopen(const char *name, char *mode);
 
 int main(){
+	FILE *f = fopen("test.txt", "r");
+	int c = 0;
+	while((c = getc(f)) > 0)
+		_flushbuf(c, stdout);
+
+	return 0;
 }
 
 #include <unistd.h>
@@ -67,7 +69,7 @@ FILE* fopen(const char *name, char *mode)
 		return NULL;
 
 	for(fp = _iob; fp < _iob + OPEN_MAX; fp++)
-		if((fp->flag & (_READ | _WRITE)) == 0)
+		if(!fp->flags.read && !fp->flags.write)
 			break;
 	if(fp >= _iob + OPEN_MAX) /* no free slots */
 		return NULL;
@@ -87,12 +89,60 @@ FILE* fopen(const char *name, char *mode)
 	fp->fd = fd;
 	fp->ctn = 0;
 	fp->base = NULL;
-	fp->flag = (*mode == 'r') ? _READ : _WRITE;
+
+	if(*mode == 'r')
+		fp->flags.read = 1;
+	else
+		fp->flags.write = 1;
+
 	return fp;
 }
 
 /* _fillbuf: allocate and fill input buffer*/
 int _fillbuf(FILE *fp)
 {
+	if(fp->flags.eof)
+		return EOF;
+
+	size_t bufsize = fp->flags.unbuf ? 1 : BUFSIZE;
+
+	if(!fp->base && !(fp->base = (char*)malloc(bufsize)))
+		return EOF;
+
+	fp->ptr = fp->base;
+
+	fp->ctn = read(fp->fd, fp->ptr, BUFSIZE);
+
+	if(--fp->ctn <= 0)
+	{
+		if(fp->ctn == -1)
+			fp->flags.eof = 1;
+		else
+			fp->flags.err = 1;
+
+		fp->ctn = 0;
+
+		return EOF;
+	}
+
+
+	return *(fp->ptr++);
 }
+
+int _flushbuf(int x, FILE *fp)
+{
+	//NOTE: Will be implemented in exercise 8-3
+	// for now, just write
+	write(fp->fd, &x, 1);
+
+	return x;
+}
+
+FILE _iob[OPEN_MAX] = {
+	{0, NULL, NULL, {1/*read*/, 0 , 1/*unbuf*/}, 0},
+	{0, NULL, NULL, {0, 1/*write*/, 0}, 1},
+	{0, NULL, NULL, {0, 1, 1/*unbuf*/}, 2},
+};
+
+
 
