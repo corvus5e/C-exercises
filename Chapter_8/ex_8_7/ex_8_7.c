188771339678f6f34793af73d1f3c146e3d51456
corvus5e <corvus5e@gmail.com>
2025-05-24 21:43:06 +0200

    Adding exercise 8-7 task description

--- Chapter_8/ex_8_7/ex_8_7.c
+++ Chapter_8/ex_8_7/ex_8_7.c
@@ -1,5 +1,7 @@
 /*
- * Exercise 8-7. 
+ * Exercise 8-7. malloc accepts a size request without checking its plausibility;
+ * free believes that the block it is asked to free contains a valid size field.
+ * Improve these routines so they make more pains with error checking.
 */
 
 #define _GNU_SOURCE 1 /*NOTE: to avoid  warning: implicit declaration of function ‘sbrk’
